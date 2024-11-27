01d24e5d525972d5b0a5a7ab520a19c22afa21c2
Lucas Briesemeister <lucas.briesemeister@totvs.com.br>
2024-11-27 11:37:57 -0300

    feat: optimizations

--- letters-combinations-phone-number/index.ts
+++ letters-combinations-phone-number/index.ts
@@ -9,40 +9,29 @@ const LETTERS_DICT = new Map([
   ["9", ["w", "x", "y", "z"]],
 ]);
 
+
 function letterCombinations(digits: string) {
   if (!digits) return [];
   const letters = digits.split("").map((d) => LETTERS_DICT.get(d)!);
   const result = findCombination(letters);
-  return chunk(result, digits.length);
+  return result;
 }
 
 function findCombination(
   letters: string[][],
   currentIndex = 0,
-  previousCombination: string = ""
-): string {
-
-  if (currentIndex >= letters.length) return ''
-
-  let fullCombination = "";
+  previousCombination: string = "",
+  result: string[] = []
+) {
+  if (currentIndex >= letters.length) return "";
 
   for (const letter of letters[currentIndex]) {
     const combination = previousCombination + letter;
-    fullCombination += findCombination(letters, currentIndex + 1, combination) || combination;
+    findCombination(letters, currentIndex + 1, combination, result) || combination;
+    if (currentIndex === letters.length - 1) result.push(combination);
   }
 
-  return fullCombination;
-}
-
-function chunk(fullText: string, chunkSize: number) {
-  const numChunks = Math.ceil(fullText.length / chunkSize);
-  const chunks = new Array<string>(numChunks);
-
-  for (let i = 0, chunk = 0; i < numChunks; i++, chunk += chunkSize) {
-    chunks[i] = fullText.substring(chunk, chunk + chunkSize);
-  }
-
-  return chunks;
+  return result
 }
 
 console.log(letterCombinations("234"));
